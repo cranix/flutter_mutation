d22d7804922b98eb1577879db25b6c24fe2c99f9
nick <nick@cleaninglab.co.kr>
2024-07-21 10:50:33 +0900

    mutation of 추가.

--- lib/mutation_extensions.dart
+++ lib/mutation_extensions.dart
@@ -3,12 +3,22 @@ import 'dart:async';
 import 'package:flutter_mutation/mutation_key.dart';
 
 extension MutationsFutureExtension<R> on Future<R> {
+  Future<R> mutateNowOf(String of, {bool append = false}) {
+    final key = MutationKey.of<R>(of);
+    return key.mutateNow(this, append: append);
+  }
+
   Future<R> mutateNow(MutationKey<R> retainKey, {bool append = false}) {
     return retainKey.mutateNow(this, append: append);
   }
 }
 
 extension MutationsFutureOrExtension<R> on FutureOr<R> {
+  Future<R> mutateNowOf(String of, {bool append = false}) {
+    final key = MutationKey.of<R>(of);
+    return key.mutateNow(this, append: append);
+  }
+
   FutureOr<R> mutateNow(MutationKey<R> retainKey, {bool append = false}) {
     return retainKey.mutateNow(this, append: append);
   }
